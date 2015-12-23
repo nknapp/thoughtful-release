a74cfe57aa8ddd46139ffb44da32771c24e2e724
Nils Knappmeier <n.knappmeier@i-views.de>
2015-12-23 22:28:10 +0100

    Initial check-in

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * thoughtful-release <https://github.com/nknapp/thoughtful-release>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = function thoughtfulRelease () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * thoughtful-release <https://github.com/nknapp/thoughtful-release>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var thoughtfulRelease = require('../')
+
+describe('thoughtful-release:', function () {
+  // body
+})
